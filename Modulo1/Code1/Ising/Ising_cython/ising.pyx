4c759920e4ea9b74360259059073e13a1237ce26
dodogabrie <dodogabrie97@live.it>
2021-10-04 20:02:57 +0200

    Documentation of the ising in cyhton

--- Modulo1/Code1/Ising/Ising_cython/ising.pyx
+++ Modulo1/Code1/Ising/Ising_cython/ising.pyx
@@ -9,10 +9,38 @@ from libc.math cimport exp
 def do_calc(int nlat, int iflag, int measures, 
             int i_decorrel, float extfield, 
             float beta, int numfile):
-    
+    """
+    Main function for the ising model in 2D:
+    Evaluate the eneggy and the magnetization of a "lattice of spin". 
+
+    Parameters
+    ----------
+    nlat : integer
+        Linear dimension of the matrix
+    iflag : integer
+        If zero start the simulation "cold", all the spin up.
+        If one start the simulation at T infinite, random spin.
+    measures : integer
+        Number of evaluation of energy and magnetization.
+    i_decorrel : integer
+        Number of iteration of the metropolis to decorrelate.
+    extfield : float
+        External magnetic field.
+    beta : float 
+        Physics quantity defined by 1/(Kb*T) with T the standard temperature.
+    numfile : integer
+        Number of the output file (for multiple parallel simulation)
+
+    Results
+    -------
+    (numpy 1d array, numpy 1d array)
+    Sampled Magnetization and Energy in two array of lenght "measures".
+    """
+    # define the random generator
     rng =  np.random.Generator(np.random.PCG64())
+    # volume of the lattice
     nvol = nlat*nlat
-    # Geometry array
+    # geometry array
     cdef np.ndarray[np.int_t, ndim=1, mode='c'] npp = np.zeros(nlat).astype(int)
     cdef np.ndarray[np.int_t, ndim=1, mode='c'] nmm = np.zeros(nlat).astype(int)
     # results array
@@ -20,38 +48,45 @@ def do_calc(int nlat, int iflag, int measures,
     cdef np.ndarray[np.double_t, ndim=1, mode='c'] ene = np.empty(measures).astype(float)
     # random array
     cdef np.ndarray[np.double_t, ndim=1, mode='c'] rr = np.empty(3*i_decorrel*nlat*nlat).astype(float)
-    # matrix array
+    # lattice array
     cdef np.ndarray[np.int_t, ndim=2, mode='c'] field = np.ones((nlat, nlat)).astype(int)
 
     cdef int i, idec # index for loops
     
-    geometry(nlat, npp, nmm)
-    inizialize_lattice(iflag, nlat, field)
+    geometry(nlat, npp, nmm) # Set the boundary conditions
+    inizialize_lattice(iflag, nlat, field) # Inizialize the lattice
 
     for i in range(measures):
-        rr = rng.uniform(size = 3*i_decorrel*nlat*nlat)
+        rr = rng.uniform(size = 3*i_decorrel*nlat*nlat) # Extract the random points for the MC
         for idec in range(i_decorrel):
-            update_metropolis(field, nlat, npp, nmm, beta, extfield, rr, idec*i_decorrel)
-        magn[i] = magnetization(field, nlat, nvol)
-        ene[i]  = energy(field, extfield, nlat, nvol, npp, nmm)
+            update_metropolis(field, nlat, npp, nmm, beta, extfield, rr, idec*i_decorrel) # MC
+        magn[i] = magnetization(field, nlat, nvol) # Compute magnetization
+        ene[i]  = energy(field, extfield, nlat, nvol, npp, nmm) # Compute energy
 
-    np.savetxt('lattice', field, fmt='%0.f')
-    np.savetxt(f'data/data{numfile}.dat', np.column_stack((magn, ene)))
+    np.savetxt('lattice', field, fmt='%0.f') # Save the lattice in file
+    np.savetxt(f'data/data{numfile}.dat', np.column_stack((magn, ene))) # Save Energy and Magnetization
     return magn, ene
 
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexing.
 cdef void geometry(int nlat, np.int_t[:] npp, np.int_t[:] nmm):
+    """
+    Set the boundary conditions on the lattice: in this case the conditions 
+    are periodic.
+    """
     cdef int i
     for i in range(nlat):
         npp[i] = i + 1
         nmm[i] = i - 1
     npp[nlat-1] = 0
     nmm[0] = nlat-1
+
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexing.
 cdef void inizialize_lattice(int iflag, int nlat, np.int_t[:,:] field):
-
+    """
+    Inizialize lattice values (to 1 or -1 like the spin up/down)
+    """
     rng =  np.random.Generator(np.random.PCG64())
 
     cdef float x
@@ -76,6 +111,9 @@ cdef void inizialize_lattice(int iflag, int nlat, np.int_t[:,:] field):
 @cython.wraparound(False)   # Deactivate negative indexing.
 @cython.cdivision(True)   # Deactivate negative indexing.
 cdef double magnetization(np.int_t[:,:] field, int nlat, int nvol):
+    """
+    Compute the magnetization of the system as (sum of the spin)/Volume
+    """
     cdef int i, j
     cdef double xmagn = 0.
     for i in range(nlat):
@@ -88,6 +126,10 @@ cdef double magnetization(np.int_t[:,:] field, int nlat, int nvol):
 @cython.cdivision(True)   # Deactivate negative indexing.
 cdef double energy(np.int_t[:,:] field, float extfield, int nlat, int nvol, 
                    np.int_t[:] npp, np.int_t[:] nmm):
+    """
+    Compute the energy of the system as 
+        E = [0.5 * sum(neighboor * spin) - sum(extfield * spin)]/Volume
+    """
     cdef int i, j, force
     cdef double xene = 0.
     for i in range(nlat):
@@ -104,6 +146,9 @@ cdef inline void update_metropolis(np.int_t[:,:] field, # the field
                                    np.int_t[:] npp, np.int_t[:] nmm, # geometry arrays
                                    float beta, float extfield, # simulation parameters
                                    np.double_t[:] rr, int skip): # random numbers parameters
+    """
+    Update the lattice with a metropolis.
+    """
     cdef int ivol, i, j, phi
     cdef float force
     for ivol in range(nlat*nlat):
@@ -120,6 +165,9 @@ cdef inline void update_metropolis(np.int_t[:,:] field, # the field
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexing.
 cdef int neigh_force(int i, int j, np.int_t[:,:] field, np.int_t[:] npp, np.int_t[:] nmm):
+    """
+    Compute the neighboors force
+    """
     cdef int ip, im, jp, jm
     ip = npp[i]
     im = nmm[i]
