e4a835b583b8a7e18411c7d4eb03c46edf8a15c5
dodogabrie <dodogabrie97@live.it>
2021-10-06 00:49:04 +0200

    Updated error with correlation

--- Modulo1/Code1/Metropolis_Gauss/mGauss_cython/m_gauss.pyx
+++ Modulo1/Code1/Metropolis_Gauss/mGauss_cython/m_gauss.pyx
@@ -13,28 +13,26 @@ def do_calc(int nstat, int start, float aver,
     rng =  np.random.Generator(np.random.PCG64())
 
     cdef np.ndarray[DTYPE_t, ndim=1, mode='c'] val_arr = np.empty(nstat)
-    cdef np.ndarray[DTYPE_t, ndim=1, mode='c'] rand_arr = rng.uniform(size = 2*nstat)
+    cdef np.ndarray[DTYPE_t, ndim=2, mode='c'] rand_arr = rng.uniform(size = (nstat, 2))
     cdef np.ndarray[np.int_t, ndim=1, mode='c'] acc_arr = np.empty(nstat).astype(int)
 
     metro_loop(nstat, delta, aver, sigma, val_arr, rand_arr, acc_arr)
     np.savetxt(f'data/data.dat', np.column_stack((val_arr, acc_arr))) # Save Energy and Magnetization
     return val_arr, acc_arr
 
-
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 @cython.cdivision(True)  
 cdef void metro_loop(int nstat, float delta, float aver, float sigma, 
-                     DTYPE_t[:] val_arr, DTYPE_t[:] rand_arr, np.int_t[:] acc_arr):
-    cdef int i, j, acc
+                     DTYPE_t[:] val_arr, DTYPE_t[:, :] rand_arr, np.int_t[:] acc_arr):
+    cdef int i, acc
     cdef float z, q_try, q = 0
     for i in range(nstat):
-        j = 2*i
 
-        q_try = q + delta * ( 1. - 2 * rand_arr[j] )
-        z = exp(( q - aver )**2 - ( q_try - aver )**2)/(2. * sigma)
+        q_try = q + delta * ( 1. - 2 * rand_arr[i, 0] )
+        z = exp((( q - aver )**2 - ( q_try - aver )**2)/(2. * sigma))
         
-        if rand_arr[j + 1] < z:
+        if rand_arr[i, 1] < z:
             q = q_try
             acc = 1
         else: acc = 0
--- Modulo1/Code1/Metropolis_Gauss/mGauss_cython/test.py
+++ Modulo1/Code1/Metropolis_Gauss/mGauss_cython/test.py
@@ -3,6 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time 
 
+def dfi_dfik(x, k, N, mean_x):
+    return (x[ 0 : N-k ] - mean_x) * (x[ k :  N  ] - mean_x)
+
+def C(x, kmax):
+    N = len(x)
+    Ck = np.empty(kmax-1)
+    mean_x = np.mean(x)
+    Ck = [1/(N-k) * np.sum(dfi_dfik(x, k, N, mean_x)) for k in range(1, kmax)]
+    return Ck
+
+def err_corr(x, kmax):
+    Ck = C(x, kmax)
+    tau = np.sum(Ck)
+    print(tau)
+    return err(x) * np.sqrt(1 + 2*tau), Ck
+    
+
 def err(X):
     N = float(len(X))
     return np.sqrt( 1/N * 1/(N-1) * np.sum((X-np.mean(X))**2))
@@ -17,7 +34,26 @@ start = time.time()
 arr, acc = m_gauss.do_calc(nstat, start, aver, sigma, delta)
 print(time.time()-start)
 
-print(f'Mean: {np.mean(arr):.6f} +- {err(arr):.6f}')
+arr = arr[int(nstat/4): ]
 
-plt.hist(arr, bins = 100)
+kmax = 2000
+start = time.time()
+e_corr, Ck = err_corr(arr, kmax)
+print(time.time()-start)
+
+print(f'Mean: {np.mean(arr):.6f} +- {err(arr):.6f}')
+print(f'Mean: {np.mean(arr):.6f} +- {e_corr:.6f}')
+
+fig, axs = plt.subplots(1, 2)
+
+axs[0].hist(arr, bins = 50)
+axs[0].set_xlabel('x')
+axs[0].set_ylabel('count')
+
+axs[1].plot(arr)
+axs[1].set_xlabel('MC step')
+axs[1].set_ylabel('MC story')
+
+plt.show()
+plt.plot(Ck)
 plt.show()
