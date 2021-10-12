757baec888eb6c998464d011ebf8cee6d35884fb
dodogabrie <dodogabrie97@live.it>
2021-10-13 00:53:14 +0200

    added fancy plot for waves

--- Modulo4/Code4/Mannella/Lax/LAX.py
+++ Modulo4/Code4/Mannella/Lax/LAX.py
@@ -1,11 +1,21 @@
 import numpy as np
 from numba import jit_module
 
+def LAX_complete(evo, u, Nt, alpha, ninner):
+    evo[0] = u
+    for i in range(Nt-1):
+        u = LAX(u, alpha, ninner)
+        evo[i+1] = u
+    return evo
+
 def LAX(u, alpha, ninner):
     for i in range(ninner):
-        u[1:-1] = 1/2 * (u[2:] * ( 1 - alpha ) + u[:-2] * ( 1 + alpha ))
-        u[0] = 1/2 * (u[1] * ( 1 - alpha ) + u[-2] * ( 1 + alpha ))
-        u[-1] = 1/2 * (u[0] * ( 1 - alpha ) + u[-2] * ( 1 + alpha ))
+        u[1:-1] = LAX_step(u[:-2], u[2:], alpha)
+        u[0] = LAX_step(u[-1], u[1], alpha)
+        u[-1] = LAX_step(u[-2], u[0], alpha)
     return u
+
+def LAX_step(u_prev, u_next, alpha):
+    return 1/2 * (u_next * ( 1 - alpha ) + u_prev * ( 1 + alpha ))
 
 jit_module(nopython = True, cache = True, fastmath = True)
--- Modulo4/Code4/Mannella/Lax/test.py
+++ Modulo4/Code4/Mannella/Lax/test.py
@@ -1,45 +1,34 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from numba import njit
 import LAX as lax
+import plot_wave
 
 def test_func(u):
     return np.exp( - u**2/2 ) * np.cos(2 * np.pi * u)
 
 def test():
 
-    v = 10
+    v = -1
     dt = 0.002
-    dx = 0.02
+    dx = 0.03
     Nt = 100
-    n = 200
-    ninner = 1
+    n = 50
+    ninner = 2
 
     alpha = v * dt /dx
     print(alpha)
 
     x = np.linspace(- n/2 * dx, n/2 * dx, n)
+    t = np.linspace(0, dt * Nt, Nt)
+
     u = test_func(x)
-    
-    #### Animation stuff
-    def animate(i):
-        # LAX goes directly here
-        line.set_ydata( lax.LAX(u, alpha, ninner) )  # update the data
-        return line,
-    
-    def init():
-        line.set_ydata(np.ma.array(x, mask=True))
-        return line,
-    #####
-    
-    # Plot results
-    fig, ax = plt.subplots()
-    line, = ax.plot(x, np.sin(x))
-    ani = animation.FuncAnimation(fig, animate, np.arange(1, Nt), init_func=init,
-                                  interval=25, blit=True)
-    plt.show()
+    uinit = np.copy(u)
+
+
+    plot_wave.animated_full(lax.LAX, lax.LAX_complete, x, t, uinit, alpha, ninner, imported_title = 'Evoluzione Metodo LAX')
+#    plot_wave.animated_basic(x, uinit, lax.LAX, Nt, alpha, ninner) 
+
 
 if __name__ == '__main__':
     test()
