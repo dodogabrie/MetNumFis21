0c3e1244e384f06b2f1ff285a39d35c953fe82c3
dodogabrie <dodogabrie97@live.it>
2021-10-15 00:30:40 +0200

    added folder for usefull codes

---
+++ utils/m1/bootstrap.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numba import jit_module
+#from numpy.random import default_rng
+
+def bootstrap(arr, M, estimator):
+    N = len(arr)
+    rest = N%M
+    arr_new = np.empty(N-rest)
+    fake_estimator = np.empty(M)
+    xx = np.arange(M)
+    k = int(N/M)
+    for j in range(M):
+        for i in range(k):
+            N_rnd = int(np.random.rand()*k)
+            arr_new[i*M : (i+1)*M] = arr[ N_rnd*M : (N_rnd + 1) * M ]
+        fake_estimator[j] = estimator(arr_new)
+    error = np.std(fake_estimator)
+    return error
+
+jit_module(nopython=True, fastmath=True)
