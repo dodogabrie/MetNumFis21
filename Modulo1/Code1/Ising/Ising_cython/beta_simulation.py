3ff6c0cd1988385a3c91f31bc8bc81a771ef27d6
dodogabrie <dodogabrie97@live.it>
2021-10-18 17:36:39 +0200

    Parallelized beta loop

--- Modulo1/Code1/Ising/Ising_cython/beta_simulation.py
+++ Modulo1/Code1/Ising/Ising_cython/beta_simulation.py
@@ -17,40 +17,31 @@ import ising
 import time 
 from numba import njit
 import joblib
+from joblib import Parallel, delayed
+###
 
 def beta_loop(iflag, nlat, beta_array, 
               measures, i_decorrel, 
-              extfield, M, njobs):
-    # Define quantities
-    chi_array = np.empty(len(beta_array))
-    dchi_array = np.empty(len(beta_array))
-    c_array = np.empty(len(beta_array))
-    dc_array = np.empty(len(beta_array))
-    m_abs_array = np.empty(len(beta_array))
-    dm_abs_array = np.empty(len(beta_array))
-    ene_array = np.empty(len(beta_array))
-    dene_array = np.empty(len(beta_array))
-    ####
-    for i, beta in enumerate(beta_array):
-        print(f'L: {nlat}, step {i} su {len(beta_array)}', end='\r')
+              extfield, M, n_jobs):
+    def parallel_job(i, beta):
+        print(f'L: {nlat}, step {i} su {len(beta_array)}')
         magn, ene = ising.do_calc(nlat, iflag, measures, i_decorrel, extfield, beta)
         m_abs = np.abs(magn)
-        chi_array[i] = compute_chi(m_abs, (nlat, beta))
-        c_array[i] = compute_c(ene, (nlat, beta))
-        dchi_array[i] = bootstrap_corr(m_abs, M, compute_chi, param = (nlat, beta))
-        dc_array[i]   = bootstrap_corr(ene  , M, compute_c  , param = (nlat, beta))
-        m_abs_array[i] = np.mean(m_abs)
-        dm_abs_array[i] = err_naive(m_abs)
-        ene_array[i] = np.mean(ene)
-        dene_array[i] = err_naive(ene)
-    ####
-    np.savetxt(f'data/data_obs_nlat{nlat}.dat', 
-               np.column_stack((beta_array, m_abs_array, dm_abs_array, 
-                                            ene_array  , dene_array  ,
-                                            chi_array  , dchi_array  ,
-                                            c_array    , dc_array     )
-                              )
-               ) 
+        chi = compute_chi(m_abs, (nlat, beta))
+        c = compute_c(ene, (nlat, beta))
+        dchi = bootstrap_corr(m_abs, M, compute_chi, param = (nlat, beta))
+        dc = bootstrap_corr(ene  , M, compute_c  , param = (nlat, beta))
+        m_abs_mean = np.mean(m_abs)
+        dm_abs = err_naive(m_abs)
+        ene_mean = np.mean(ene)
+        dene = err_naive(ene)
+        return [beta, m_abs_mean, dm_abs, ene_mean, dene, chi, dchi, c, dc] 
+
+    list_args = [[i, beta] for i, beta in enumerate(beta_array)]
+    list_outputs = Parallel(n_jobs=n_jobs)(delayed(parallel_job)(*args) for args in list_args)
+
+    np.savetxt(f'data/data_obs_nlat{nlat}.dat', np.array(list_outputs)) 
+
     return 
 
 def L_loop(iflag, L_array, beta_array, 
@@ -60,17 +51,16 @@ def L_loop(iflag, L_array, beta_array,
         beta_loop(iflag, nlat, beta_array, 
               measures, i_decorrel, 
               extfield, M, njobs)
-        return
+    return
 
 if __name__ == '__main__':
     iflag = 1
-    nlat = 10
-    beta = np.linspace(0.36, 0.48, 96)
-    L    = np.arange(10, 40, 10, dtype = int)
+    beta_array = np.linspace(0.36, 0.48, 50)
+    L_array    = np.arange(20, 50, 10, dtype = int)
     measures = int(1e5)
-    i_decorrel = 50
+    i_decorrel = 20
     extfield = 0.
     M = 2000
     start = time.time()
-#    beta_loop(iflag, nlat, beta, measures, i_decorrel, extfield, M)
+    L_loop(iflag, L_array, beta_array, measures, i_decorrel, extfield, M, njobs = 8)
     print('\n', time.time()-start)
