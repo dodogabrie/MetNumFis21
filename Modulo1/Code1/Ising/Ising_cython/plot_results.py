e39f86d8d66bc127a04efe316c7bffe7bb648378
dodogabrie <dodogabrie97@live.it>
2021-10-19 10:40:00 +0200

    Commented the code for better understanding

--- Modulo1/Code1/Ising/Ising_cython/plot_results.py
+++ Modulo1/Code1/Ising/Ising_cython/plot_results.py
@@ -1,15 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#%%%%%%%%%% Parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Data to esplore divided by lateral size of grid
 nlats = [10,20, 30, 40, 50, 60]
-list_data = [f'data/data_obs_nlat{n}_test.dat' for n in np.array(nlats)]
-dict_val = dict(magn = [1, 2], ene = [3, 4], chi = [5, 6], c = [7, 8])
+
+# Quantity to estimate 
 estimate = 'chi'
-i, di = dict_val[estimate]
+#estimate = 'ene'
+#estimate = 'magn'
+#estimate = 'c'
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Extract list of data files
+list_data = [f'data/data_obs_nlat{n}_test.dat' for n in np.array(nlats)]
+# Dictionary for indices of searched quantities
+dict_val = dict(magn = [1, 2], ene = [3, 4], chi = [5, 6], c = [7, 8])
+
+i, di = dict_val[estimate] # Extract indices from dict
+
+# For every dataset (one for each nlat) plot the curve varing beta
 for d, l in zip(list_data, nlats):
-    
-    data = np.loadtxt(d)
-    x = data[:,0]
+    data = np.loadtxt(d) # Extract data
+    x = data[:,0] # X coordinate is always beta
     y = data[:,i]
     plt.errorbar(x, y, yerr = data[:,di], label = f'idec = 100, L {l}')
     
--- Modulo1/Code1/Ising/Ising_cython/test_obs.py
+++ Modulo1/Code1/Ising/Ising_cython/test_obs.py
@@ -17,20 +17,26 @@ from os.path import isfile, join
 from joblib import Parallel, delayed
 
 def test():
+    #%%%%%%%%%%% Parameters %%%%%%%%%%%%%
     nlat = 20
-    n_jobs = 8
-    M = 2000 #sicuri?
+    M = 2000 # sicuri? --> incide solo sull'errore...
     data_dir = "data/nlat20/"
+    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    # Extract all file names in the folder
     onlyfiles = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
-   
+    
+    # Define the function that extract data in list
     def extract_obs(data_name):
-        # Extract beta from filename 
-        file_test = data_name
-        tmp_string = file_test.split('_nlat')[0]
-        beta = float(tmp_string.split('beta')[1])
- 
+        # Extract beta from file name 
+        tmp_string = data_name.split('_nlat')[0] # beta comes first of '_nlat'
+        beta = float(tmp_string.split('beta')[1])# beta comes after 'beta'
+        
+        # Load all the magnetization and energy at fixed beta
         data = fastload(data_name.encode('UTF-8'), int(1e5))
         magn, ene = data[:, 0], data[:, 1]
+        
+        # Compute all quantities
         m_abs = np.abs(magn)
         chi = compute_chi(magn, (nlat, beta))
         c = compute_c(ene, (nlat, beta))
@@ -40,6 +46,7 @@ def test():
         dm_abs = err_naive(m_abs)
         ene_mean = np.mean(ene)
         dene = err_naive(ene)
+        # Returns all the quantities in a list
         return [beta, m_abs_mean, dm_abs, ene_mean, dene, chi, dchi, c, dc]
 
     list_outputs = [extract_obs(data_dir + data_name) for data_name in onlyfiles]
