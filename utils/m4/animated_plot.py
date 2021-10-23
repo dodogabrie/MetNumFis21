8a7d0cee5e46d6f8aff5b872671d4147472892b8
Sara Ardito <sarah02_ardito@hotmail.it>
2021-10-23 09:51:30 +0200

    some changes

--- utils/m4/animated_plot.py
+++ utils/m4/animated_plot.py
@@ -4,7 +4,7 @@ import matplotlib.animation as animation
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-def animated_basic(x, uinit, func, Nt, *param, plot_dim = None):
+def animated_basic(x, uinit, func, Nt, *param, plot_dim = None, dilat_size = 0.2):
     """
     Return a matplotlib 2D animation of the PDE evolution.
 
@@ -28,6 +28,9 @@ def animated_basic(x, uinit, func, Nt, *param, plot_dim = None):
     plot_dim : int or None
         The dimension to plot in case of multidimensional problem. If None
         the problem is assumed 1D.
+    dilat_size : float
+        The dilatation of the ylim respect to the max and the min of the 
+        function at initial time.
     
     Returns
     -------
@@ -48,13 +51,141 @@ def animated_basic(x, uinit, func, Nt, *param, plot_dim = None):
     u1 = np.copy(uinit)
     fig, ax1 = plt.subplots()
     if plot_dim == None:
+        min_u = np.min(uinit)
+        max_u = np.max(uinit)
         line, = ax1.plot(x, uinit)
     else:
+        min_u = np.min(uinit[plot_dim])
+        max_u = np.max(uinit[plot_dim])
         line, = ax1.plot(x, uinit[plot_dim])
+
+    size = (max_u - min_u)*dilat_size
+    ax1.set_ylim(min_u - size, max_u + size)
 
     ani = animation.FuncAnimation(fig, animate, np.arange(0, Nt), init_func=init, fargs = (plot_dim,),
                                   interval=25, blit=True)
     plt.show()
+
+def animated_with_slider(x, uinit, func, Nt, dt, *param, plot_dim = None, dilat_size = 0.1, imported_title = None):
+    """
+    Return a matplotlib 2D animation of the PDE evolution.
+
+    Parameters
+    ----------
+    x : 1d numpy array
+        Spatial grid of the system.
+    uinit : 1d numpy array.
+        Initial condition of the PDE (a function evaluated on x).
+    func : function
+        Function that evaluate the next temporal step of the solution u.
+        Here you can pass the implementation of the LAX methods.
+        The parameters of the function needs to be: func(u, *param) where
+        u is the solution at time i and *param are the other parameters of the 
+        system (like alpha and ninner for the implemented LAX).
+    Nt : int
+        Number of temporal step for the evolution of the PDE.
+    dt : float
+        Temporal step of the simulation.
+    *param : *params argument
+        This values will be passed to func, for example you can put here
+        alpha, ninner in the case of the LAX method.
+    plot_dim : int or None
+        The dimension to plot in case of multidimensional problem. If None
+        the problem is assumed 1D.
+    dilat_size : float
+        The dilatation of the ylim respect to the max and the min of the 
+        function at initial time.
+    imported_title: string
+        Title of the figure.
+    
+    Returns
+    -------
+    """
+
+    if imported_title == None:
+        imported_title = 'Animated plot'
+
+    duration = Nt
+
+    u = np.copy(uinit)
+
+    def my_scatter(x, func, u, plot_dim, *param):
+        if plot_dim == None: return go.Scatter(x = x, y = func(u, *param), mode = "lines+markers", marker=dict(size=6))
+        else: return go.Scatter(x = x, y = func(u, *param)[plot_dim], mode = "lines+markers", marker=dict(size=6))
+
+    ######### Definire una figure con Plolty
+    if plot_dim == None: init_scatter = go.Scatter( x=x, y=uinit, mode='lines+markers', marker=dict(size=6))
+    else: init_scatter = go.Scatter( x=x, y=uinit[plot_dim], mode='lines+markers', marker=dict(size=6))
+
+    fig = go.Figure(data = init_scatter)
+    fig.update_xaxes(title_text = "x")
+    fig.update_yaxes(title_text = "u")
+    if plot_dim == None:
+        m = np.min(uinit)
+        M = np.max(uinit)
+    else:
+        m = np.min(uinit[plot_dim])
+        M = np.max(uinit[plot_dim])
+    dilat = (M - m) * dilat_size
+    fig.update_layout(yaxis_range=[m - dilat , M + dilat ])
+ 
+
+    ## Evolving the frames ###############################################
+    my_frames = [go.Frame(data = [my_scatter(x, func, u, plot_dim, *param)],
+                name=str(i), layout_yaxis_range=[m - dilat , M + dilat])
+                for i in range(Nt)]
+
+    fig.frames = my_frames
+
+    ## Setting dello slider ##
+    def frame_args(duration):
+        return dict(frame={"duration": duration},
+                    mode ="immediate",
+                    fromcurrent=True,
+                    transition={"duration": duration, 
+                                "easing": "linear"},)
+    # Definire i parametri dello slider 
+    sliders = [{"pad": {"b": 10, "t": 60},
+                "len": 0.5,
+                "x": 0.1,
+                "y": 0,
+                "steps": [{"args": [[f.name], frame_args(duration*dt)],
+                           "label": 't = ' + str(k*dt),
+                           "method": "animate",}
+                    for k, f in enumerate(fig.frames)],}]
+
+    ## Layout generale della figura ##
+    fig.update_layout(
+         title=imported_title,
+         width=1200,
+         height=700,
+         scene=dict(
+             xaxis_title='x',
+             yaxis_title='t',
+             zaxis_title='u',
+             aspectratio=dict(x=1, y=1, z=1)),
+         updatemenus = [
+                {
+                "buttons": [
+                    {"args": [None, frame_args(duration)],
+                     "label": "&#9654;", # play symbol
+                     "method": "animate",},
+                    {"args": [[None], frame_args(duration)],
+                     "label": "&#9724;", # pause symbol
+                     "method": "animate",
+                    },],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+         ],
+         sliders=sliders
+    )
+
+    fig.show()
+    return 
 
 
 def animated_full(func, func_mesh, x, t, uinit, *param, imported_title = None, plot_dim = None):
---
+++ utils/m4/derivate.py
@@ -0,0 +1,62 @@
+"""
+Evaluate the derivative of a function in 3 different way using numpy vectorial
+operations. 
+The efficient one ( o[(dx)^2] ) is simm_der (the last one), the other 2 methods
+are just o[dx].
+"""
+
+import numpy as np
+#from numba import jit_module
+
+def foward_der(x):
+    """
+    Derivative considering the next point
+    """
+    der = np.empty(len(x))
+    dx = x[1]-x[0]
+    der[:-1] = (x[1:] - x[:-1])/dx
+    der[-1] = der[-2]
+    return der
+
+def backward_der(x):
+    """
+    Derivative considering the previous point
+    """
+    der = np.empty(len(x))
+    dx = x[1]-x[0]
+    der[1:] = (x[1:] - x[:-1])/dx
+    der[0] = der[1]
+    return der
+
+def simm_der(f, dx, out):
+    """
+    Derivative considering both the next and the previous points
+    """
+    bound0, bound1 = f[0], f[-1]
+    bound_next0, bound_next1 = f[1], f[-2]
+    out[1:-1] = (f[2:] - f[:-2])/(2*dx)
+    out[0] = (bound_next0 - bound1)/(2*dx)
+    out[-1] = (bound0 - bound_next1)/(2*dx)
+    return out
+
+def simm_der2(f, dx, out):
+    """
+    Simmetrical second derivative
+    """
+    b0, b1 = f[0], f[-1]
+    bn0, bn1 = f[1], f[-2]
+    out[1:-1] = (f[2:] - 2 * f[1:-1] + f[:-2])/(dx**2)
+    out[0] = (bn0 - 2 * f[0] +  b1)/(dx**2)
+    out[-1] = (b0 - 2 * f[-1] + bn1)/(dx**2)
+    return out
+
+def shift_test(f, shift_f):
+    """
+    Testing the continuity of a function under shift
+    """
+    N = len(f)
+    quarter = int(len(f)/4)
+    remain = N-quarter
+    shift_f[:quarter] = f[-quarter:]
+    shift_f[-remain:] = f[:remain]
+    return shift_f
