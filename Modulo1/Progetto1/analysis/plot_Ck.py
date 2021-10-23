319590adeb7359affb710ab10bdc51bc7b855dcb
dodogabrie <dodogabrie97@live.it>
2021-10-23 18:57:05 +0200

    Emergency sae for Sara

--- Modulo1/Progetto1/analysis/plot_Ck.py
+++ Modulo1/Progetto1/analysis/plot_Ck.py
@@ -12,39 +12,87 @@ sys.path.append(path.split(main_folder)[0] + main_folder + 'utils/')
 
 import numpy as np
 from m1.error import err_mean_corr, err_naive, bootstrap_corr
-import matplotlib.pyplot as plt
+from m1.readfile import slowload, fastload
+from os import listdir
+from os.path import isfile, join
 import time
 
-def Ck_plot(list_file):
+def Ck_plot(list_file, data_dir, M):
+    # Extract all file names in the folder
+    onlyfiles = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
+    nfile = len(onlyfiles)
+    M_arr = np.arange(M-1)
+    beta_arr = np.empty(nfile)
+    Ck_matrix = np.empty((nfile, M-1))
+    for i, data_name in enumerate(onlyfiles):
+        print(f'{i} su {nfile}', end='\r')
+        # Extract beta from file name 
+        tmp_string = data_name.split('_nlat')[0] # beta comes first of '_nlat'
+        beta = float(tmp_string.split('beta')[1])# beta comes after 'beta'
+        beta_arr[i] = beta 
+        # Load all the magnetization and energy at fixed beta
+        data = fastload((data_dir + data_name).encode('UTF-8'), int(1e5))
+        magn, ene = data[:, 0], data[:, 1]
+        tau, error, Ck = err_mean_corr(magn, M)
+        Ck_matrix[i] = Ck 
+    beta_sort = np.argsort(beta_arr)
+    beta_arr = beta_arr[beta_sort]
+    Ck_matrix = Ck_matrix[beta_sort]
 
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+    from matplotlib.cm import ScalarMappable
+
+    fig = plt.figure(figsize = (15, 5))
+    fonts = {'size' : 14}
+    plt.rc('font', **fonts)
+    plt.suptitle(r'Autocorrelazione di M al variare di $\beta$')
+    gs = GridSpec(1, 2, width_ratios=[2, 1], wspace=0.05)
+    ax1 = fig.add_subplot(gs[0])
+    ax2 = fig.add_subplot(gs[1])
+
+    X, Y = np.meshgrid(M_arr, beta_arr)
+
+    qcs = ax1.contourf(Y,X, Ck_matrix, 20, cmap = 'BuGn')
+    fig.colorbar(ScalarMappable(norm=qcs.norm, cmap=qcs.cmap), ax = ax1, pad = 0.02,)
+    ax1.set_xlabel(r'$\beta$')
+    ax1.set_ylabel('k')
+    ax1.set_xlim(0.42,0.455)
+ 
     for filename in list_file:
         # Extract beta from file name
         tmp_string = filename.split('_nlat')[0] # beta comes first of '_nlat'
         beta = float(tmp_string.split('beta')[1])# beta comes after 'beta'
 
         magn, ene = np.loadtxt(filename, unpack = True)
-        tau, error, Ck = err_mean_corr(magn, 400)
+        tau, error, Ck = err_mean_corr(magn, M)
         plt.plot(Ck, linewidth = 1, label = rf'$\beta$ = {beta:.4f}')
 
-    plt.title(rf'L = 80')
-    plt.minorticks_on()
-    plt.tick_params('x', which='major', direction='in', length=3)
-    plt.tick_params('y', which='major', direction='in', length=3)
-    plt.tick_params('y', which='minor', direction='in', length=1.5, left=True)
-    plt.tick_params('x', which='minor', direction='in', length=1.5,bottom=True)
+    ax2.set_title(rf'L = 80')
+    ax2.minorticks_on()
+    ax2.tick_params('x', which='major', direction='in', length=3)
+    ax2.tick_params('y', which='major', direction='in', length=3)
+    ax2.tick_params('y', which='minor', direction='in', length=1.5, left=True)
+    ax2.tick_params('x', which='minor', direction='in', length=1.5,bottom=True)
 
-    plt.xlabel('k')
-    plt.ylabel('C(k)')
-    plt.legend()
+    ax2.set_xlabel('k')
+    ax2.set_ylabel('C(k)')
+    ax2.legend(fontsize = 10)
 
     #plt.savefig('../figures/Ck_L80.png', dpi = 300)
     plt.show()
 
 if __name__ == '__main__':
-    L = 50
-    list_beta = [0.38, 0.4009030737312974, 0.4200588742480439, 0.4345577217609058,0.4405432022366073,
-                 0.4505067030352683, 0.4684809360062864, 0.47091621903968484, 0.48]
+
+    #list_file.append(f'../data/nlat{L}/data_beta{beta}_nlat{L}.dat')
+    M = 9000
+    L = 80
+    list_beta = [0.38, 0.4302356355217024, 0.4477279361553648, 0.48]
     list_file = []
     for beta in list_beta:
         list_file.append(f'../data/nlat{L}/data_beta{beta}_nlat{L}.dat')
-    Ck_plot(list_file)
+
+#    M = 10000
+    L = 80
+    data_dir = f'../data/nlat{L}/'
+    Ck_plot(list_file, data_dir, M)
