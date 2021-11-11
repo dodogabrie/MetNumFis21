dfb6f0251814a20ed1fe901df0a7d40e7b14cdac
dodogabrie <dodogabrie97@live.it>
2021-11-11 21:50:20 +0100

    added linear interpol of tau vs L

--- Modulo1/Progetto1/analysis/tau_int_critic.py
+++ Modulo1/Progetto1/analysis/tau_int_critic.py
@@ -37,10 +37,34 @@ def compute_z(data_main_dir):
         tau_c, _, _ = err_mean_corr(magn)
         tau_c_list.append(tau_c)
     import matplotlib.pyplot as plt
+    from sklearn.linear_model import LinearRegression
     tau_c_list = np.array(tau_c_list)
-    plt.scatter(L_array, tau_c_list)
-    plt.yscale('log')
-    plt.xscale('log')
+    tau_log = np.log(tau_c_list)
+    L_log = np.log(L_array)
+    regressor = LinearRegression().fit(L_log.reshape((len(L_log),1)), tau_log.reshape((len(tau_log),1)) )
+    z = regressor.coef_[0][0]
+    intercept = regressor.intercept_[0]
+    xx = np.linspace(np.min(L_log), np.max(L_log), 500)
+
+    fig, ax = plt.subplots()
+    ax.plot(xx, z*xx + intercept, c = 'brown', label = f'z = {z:.2f}')
+    ax.scatter(L_log, tau_log, c = 'k', s = 17)
+    ax.set_ylabel(r'$\log(\tau_c)$', fontsize = 14)
+    ax.set_xlabel(r'$\log(L)$', fontsize = 14)
+    ax.minorticks_on()
+    ax.tick_params('x', which='major', direction='in', length=4)
+    ax.tick_params('y', which='major', direction='in', length=4)
+    ax.tick_params('y', which='minor', direction='in', length=2, left=True)
+    ax.tick_params('x', which='minor', direction='in', length=2,bottom=True)
+
+    # Ax font size
+    ax.tick_params(axis='x', labelsize=10)
+    ax.tick_params(axis='y', labelsize=10)
+    ax.set_title(r"$\log(\tau_c)$ vs $\log(L)$ al punto critico", fontsize = 15)
+
+    ax.grid(alpha=0.3)
+    ax.legend(fontsize = 13, loc = 'lower right')
+    plt.savefig('../figures/tau_c_vs_L_interpolazione.png', dpi = 200)
     plt.show()
     return
 
