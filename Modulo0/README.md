5cd9208717e847e2820607a5774fd606ed20c326
dodogabrie <dodogabrie97@live.it>
2021-11-28 13:52:14 +0100

    Removed stuff of other modules

--- Modulo0/README.md
+++
@@ -1,7 +0,0 @@
-# Module 0
-
-Module for the basics of the course.
-
-- Find roots.
-- Integrate Odes.
-- Random Number Generators.
--- Modulo1/Code1/fastdataload/test.py
+++
@@ -1,9 +0,0 @@
-from core.readfile import fastload
-import time
-
-# test data
-emlFile = b"data/data.dat"
-start = time.time()
-data = fastload(emlFile, int(1e7+2000))
-print(time.time()-start)
-print(data[:20])
