8e1992834be8aaa9d2428f11f4cafb13e42a04c0
Sara Ardito <sarah02_ardito@hotmail.it>
2021-12-03 21:41:07 +0100

    reorganized code

--- Modulo4/Code4/Califano/exercises/SinCos.py
+++ Modulo4/Code4/Califano/exercises/SinCos.py
@@ -1,37 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-#####define input variable
+###############################################################
+#define input variable
 L = 10
 N = 100
 nu = 0.01
 
-#####define the dicrete interval dx
+###############################################################
+#define the dicrete interval dx
 dx = L/N
 x = np.arange(0,L,dx)
 
-#####evaluate the function u and the analitic derivative
+###############################################################
+#evaluate the function u 
 def u(x):
     return np.sin(2*np.pi*x/L)
+
+##########################DERIVATIVE############################
+#analitic derivative
 
 def u1(x):
     return 2*np.pi/L * np.cos(2*np.pi*x/L)
 
-#####save results in file txt
-np.savetxt('SinCos.txt', np.column_stack((x,u(x),u1(x))),
-            header = 'x    u   u1', fmt = '%.4f')
-
-#####verify periodicity
-#plt.plot(np.concatenate((x,x+L)),np.concatenate((u(x), u(x+L))))
-#plt.show()
-
-#####plot function u and u1
-#plt.plot(x,u(x))
-#plt.plot(x,u1(x))
-#plt.show()
-
-#####numerical derivative
+#numerical derivative
 def foward_der(u,dx):
     """
     Derivative considering the next point
@@ -60,15 +52,36 @@ def simm_der(u,dx):
     der[-1] = (u[0] - u[-2])/(2*dx) # right periodic boundary condition
     return der
 
-#####Compare numerical derivative and analitical derivative
+########################SAVE IN FILE############################
+
+#####save results in file txt
+np.savetxt('SinCos.txt', np.column_stack((x,u(x),u1(x))),
+            header = 'x    u   u1', fmt = '%.4f')
+
+
+#######################VERIFY USING PLOT#########################
+#verify periodicity of function u
+
+plt.plot(np.concatenate((x,x+L)),np.concatenate((u(x), u(x+L))))
+#plt.show()
+
+#verify periodicity of derivative
+plt.plot(np.concatenate((x,x+L)),np.concatenate((backward_der(u(x), dx), backward_der( u(x+L), dx ))))
+#plt.show()
+
+#plot function u and derivative
+plt.plot(x,u(x))
+plt.plot(x,u1(x))
+#plt.show()
+
+#####################NUMERICAL VS ANALITICAL######################
+#Compare numerical derivative and analitical derivative
 plt.plot(x, u1(x), color = 'blue')
 plt.plot(x, foward_der(u(x), dx), color = 'red')
 plt.plot(x, backward_der(u(x), dx), color = 'green')
 plt.plot(x, simm_der(u(x), dx), color = 'pink')
-plt.show()
-
-#####verify periodicity of derivative
-#plt.plot(np.concatenate((x,x+L)),np.concatenate((backward_der(u(x), dx), backward_der( u(x+L), dx ))))
 #plt.show()
+
+
 
 
