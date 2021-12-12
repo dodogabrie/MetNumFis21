029a449a5b7b8bc5578d468b31b676b09f3e254b
dodogabrie <dodogabrie97@live.it>
2021-12-12 10:30:44 +0100

    added first LAX Wend algorithm for GW

--- Modulo4/Code4/Mannella/exercises/gw.py
+++ Modulo4/Code4/Mannella/exercises/gw.py
@@ -44,7 +44,7 @@ def main():
 
     ###############Just Plot Part###########################################
 #    # Temporal evolution
-#    f = LAX(f, dx, dt, N_step, v, s)
+#    f = lax(f, dx, dt, N_step, v, s)
 #    # Plot results
 #    plt.plot(x, f_init[0], label = 'initial')
 #    plt.plot(x, f[0], label = 'final')
@@ -57,9 +57,29 @@ def main():
     lax_param = (dx, dt, n_inner, v, s) # Define paramters of LAX func in list
     #                                   # (Withou f)
     # Animated + sliders
-#    aniplt.animated_with_slider(LAX, f, x, t, lax_param, plot_dim = 0)
+#    aniplt.animated_with_slider(lax, f, x, t, lax_param, plot_dim = 0)
     # Surface plots
-#    surface_xt(LAX, f, x, t, lax_param, plot_dim = 0)
+#    surface_xt(lax, f, x, t, lax_param, plot_dim = 0)
+
+    # LAX WENDROFF ############################################################
+    ###############Just Plot Part###########################################
+#    # Temporal evolution
+#    f = lax_wendroff(f, dx, dt, N_step, v, s)
+#    # Plot results
+#    plt.plot(x, f_init[0], label = 'initial')
+#    plt.plot(x, f[0], label = 'final')
+#    plt.legend()
+#    plt.show()
+
+    ###############Animated Plot Part#######################################
+    n_inner = 10 # Step to jump
+    t = np.linspace(0, n_inner * N_step * dt, N_step)
+    lax_param = (dx, dt, n_inner, v, s) # Define paramters of LAX func in list
+    #                                   # (Withou f)
+    # Animated + sliders
+    aniplt.animated_with_slider(lax_wendroff, f, x, t, lax_param, plot_dim = 0)
+    # Surface plots
+    #surface_xt(lax_wendroff, f, x, t, lax_param, plot_dim = 0)
     return 
 
 def lax(f, dx, dt, n_step, v, s):
@@ -72,22 +92,21 @@ def lax(f, dx, dt, n_step, v, s):
         f[:, 0], f[:, -1] = f[:, -2], f[:, 1] # boundary condition (wait)
     return f
 
-#def lax_wendroff(f, dx, dt, n_step, v, s):
-#    alpha = dt/dx*0.5 # define dt/(2dx)
-##    dt2 = dt * 0.5 # Half instant
-#    f1 = f[0] # first component of f
-#    f2 = f[1] # second component of f
-#    f1_LAX = np.copy(f1)
-#    f2_LAX = np.copy(f1)
-#    for i in range(n_step):
-#        # LAX step                                                        (      h          )
-#        f1_LAX[:-1] = 0.5*(f1[1:] + f1[:-1]) - alpha*(f1[1:] - f1[:-1])  + dt*f2[:-1]
-#        f2_LAX[:-1] = 0.5*(f2[1:] + f2[:-1]) - alpha*(-f2[1:] + f2[:-1]) + dt*(v*f1[:-1] + s)
-#        # Leap Frog
-#        f1[] = f1[] - 2*alpha*(f1_LAX[]-f1_LAX[])
-#        f2[] = f2[] - 2*alpha*(f2_LAX[]-f2_LAX[])
-#        f[:, 0], f[:, -1] = f[:, -2], f[:, 1] # boundary condition (wait)
-#    return f
+def lax_wendroff(f, dx, dt, n_step, v, s):
+    alpha = dt/dx*0.5 # define dt/(2dx)
+    f1 = f[0] # first component of f
+    f2 = f[1] # second component of f
+    f1_LAX = np.empty(len(f1)-1)
+    f2_LAX = np.empty(len(f1)-1)
+    for i in range(n_step):
+        # LAX step                                                    (      h                       )
+        f1_LAX = 0.5*(f1[1:] + f1[:-1]) - alpha*(f1[1:] - f1[:-1])  + 0.5*dt*(f2[1:] + f2[:-1])
+        f2_LAX = 0.5*(f2[1:] + f2[:-1]) - alpha*(-f2[1:] + f2[:-1]) + dt*(0.5*v*(f1[1:] + f1[:-1]) + s)
+        # Leap Frog
+        f1[1:-1] = f1[1:-1] - 2*alpha*(f1_LAX[1:]-f1_LAX[:-1]) + 0.5 * dt * ( f2_LAX[1:] + f2_LAX[:-1] )
+        f2[1:-1] = f2[1:-1] - 2*alpha*(f2_LAX[1:]-f2_LAX[:-1]) + dt * (0.5 * v * (f1_LAX[1:] + f1_LAX[:-1]) + s )
+        f[:, 0], f[:, -1] = f[:, -2], f[:, 1] # boundary condition (wait)
+    return f
 
 
 if __name__ == "__main__":
