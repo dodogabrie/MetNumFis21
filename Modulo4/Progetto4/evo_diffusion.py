202e5a8f71177f6e2444038ab06e2c77a462c575
dodogabrie <dodogabrie97@live.it>
2022-02-15 16:31:47 +0100

    added some new stuff

--- Modulo4/Progetto4/evo_diffusion.py
+++ Modulo4/Progetto4/evo_diffusion.py
@@ -9,6 +9,7 @@ from scipy import fftpack
 import matplotlib.pyplot as plt
 import NumDerivative as der
 import NumIntegrator as Int
+from utils import plot_template, my_real_fft, evaluate_energy_density_spectrum
 
 # Define the right hand side term of the eq
 def F_diffusion(u, c, nu, dx):
@@ -16,23 +17,22 @@ def F_diffusion(u, c, nu, dx):
     Function of equation:
          du/dt = nu * d^2u/d^2x - c * du/dx = F(u)
     """
-    return nu * der.simm_der2(u, dx) - c * der.simm_der(u, dx)
+    return nu * der.fft_der2(u, dx) #- c * der.simm_der(u, dx)
 
 # Define the initial condition
-def u_sin_simple(x, L):
+def u_sin_simple(x,L, k = 1):
     """
     Initial condition (given by a function 'cause here evolve a function.
     """
-    k = 1
     return np.sin(k * 2*np.pi*x/L)
     
-def u_sin_hard(x, L):
+def u_sin_hard(x, L, k = 1):
     """
     Initial condition (given by a function 'cause here evolve a function.
     """
-    k1 = 1
-    k2 = 10
-    k3 = 20
+    k1 = 1*k
+    k2 = 10*k
+    k3 = 20*k
     return np.sin(k1 * 2*np.pi*x/L) + 1/5*np.sin(k2 * 2*np.pi*x/L) + 1/10*np.sin(k3 * 2*np.pi*x/L)
 
 def ampl2(int_method, u, F):
@@ -40,25 +40,16 @@ def ampl2(int_method, u, F):
     This function plot the square of the Fourier coefficient of u 
     at the initial time and after few step.
     The bigger k are the first to explode.
-
-    Parameters
-    ----------
-    int_method: function 
-        Integration method (euler or RK2).
-    u: function
-        Initial condition
-    F: function
-        Right hand side
     """
     #define input variable
     # spatial inputs
     L = 10 # Spatial size of the grid
     N = 100 # spatial step of grid
-    nu = 0.05 # useless parameter
+    nu = 0.0003 # useless parameter
     
     # temporal inputs
     dt = 0.1 # Temporal step
-    N_step = 25 # Number of Temporal steps
+    N_step = 7500 # Number of Temporal steps
     
     ###############################################################
     #define the dicrete interval dx
@@ -75,60 +66,220 @@ def ampl2(int_method, u, F):
     c = c/2
     von_neumann = nu * dt /(dx**2)
     print(f'Von Neumann factor {von_neumann}')
-    def my_real_fft(u, dx):
-        """
-        Real Fast Fourier Transform.
-        """
-        # x : 2 * pi = y : 1 ---> unitary rate
-        # => dy = dx/(2*pi)
-        dy =  dx / (2 * np.pi)
-        # fft(j) = (u * exp(-2*pi*i*j*np.arange(n)/n)).sum()
-        fft = fftpack.fft(u) # Discret fourier transform 
-        k = fftpack.fftfreq(N, dy) 
-        return fft, k
-    
+
     # Amplitude**2 of Fourier coefficients #
     for i in range(N_step):# Evolution in time
         u_t = int_method(u_t, F, dt, c, nu, dx)
-    
-    fft_u_init, k_u_init = my_real_fft(u_init, dx)# FFT of initial u 
-    fft_u, k_u = my_real_fft(u_t.real, dx) # FFT of final u 
- 
-    mod_fft2_u = (np.abs(fft_u)/N)**2 # square of module of fft final
-    mod_fft2_u_init = (np.abs(fft_u_init)/N)**2 # square of module of fft final
 
-    mask_pos_k_u = k_u >= 0 # mask for positive k final
-    mod_fft2_u = mod_fft2_u[mask_pos_k_u]
-    k_u = k_u[mask_pos_k_u]
-
-    mask_pos_k_u_init = k_u_init >= 0 # mask for positive k init
-    mod_fft2_u_init = mod_fft2_u_init[mask_pos_k_u_init]
-    k_u_init = k_u_init[mask_pos_k_u_init]
-
+    mod_fft2_u_init, k_u_init = evaluate_energy_density_spectrum(u_init, dx) 
+    mod_fft2_u, k_u = evaluate_energy_density_spectrum(u_t, dx)
    
     # Plot results
-    plt.figure(figsize=(10, 6))
-    equation = r'$\partial_t u = \nu \partial_x^2 u - c \partial_x u \ \longrightarrow$  Derivative with simm. finite difference'
-    integ_params = '\n\nIntegrated with Runge Kutta of order 2'
-    sistem_params = f'\n\nL = {L},    dx = {dx},    dt = {dt},    N step = {N_step},    c = {c},    nu = {nu}'
-    stability = f'\n\nVon Neumann Factor = {von_neumann:.2f},    CFL (c dt/dx) = {c*dt/dx}'
-    plt.suptitle(equation + integ_params + sistem_params + stability, fontsize = 15)
-    plt.subplot(121)
-    plt.plot(x, u_init, label = 'init')
-    plt.plot(x, u_t.real, label = 'final')
-    plt.xlabel('x', fontsize=15)
-    plt.ylabel('u', fontsize=15)
-    plt.legend(fontsize=13)
-    plt.subplot(122)
-    plt.scatter(k_u_init, mod_fft2_u_init, label = 'init')
-    plt.scatter(k_u, mod_fft2_u, label = 'final')
-    plt.xlabel('k', fontsize=15)
-    plt.ylabel(r'$\left|u_k\right|^2$', fontsize=15)
-    plt.legend(fontsize=13)
-    plt.yscale('log')
+    fig, axs = plot_template()
+    equation = r'$\partial_t u = \nu \partial_x^2 u - c \partial_x u \ \longrightarrow$  RK2 e differenze finite simmetriche'
+    plt.suptitle(equation, fontsize = 15)
+
+    ax = axs[0]
+    ax.plot(x, u_init, label = 'init')
+    ax.plot(x, u_t.real, label = 'final')
+    ax = axs[1]
+    ax.scatter(k_u_init, mod_fft2_u_init, label = 'init')
+    ax.scatter(k_u, mod_fft2_u, label = 'final')
     plt.tight_layout()
 #    plt.savefig('figures/6_evo_diffusion_hard_function.png', dpi = 200)
     plt.show()
+
+
+def nu_effect(int_method, u, F):
+    #define input variable
+    # spatial inputs
+    L = 10 # Spatial size of the grid
+    N = 100 # spatial step of grid
     
+    # temporal inputs
+    dt = 0.1 # Temporal step
+    N_step = 5000 # Number of Temporal steps
+    
+    ###############################################################
+    #define the dicrete interval dx
+    dx = L/N # step size of grid
+    x = np.linspace(0,L,N, endpoint = False)
+    
+    speed = dx/dt # speed given the values
+    c = speed
+    
+    # Define starting function
+    m = 1
+    u_init = u(x, L, k = m) # save initial condition 
+    u_t = np.copy(u_init) # create e copy to evolve it in time
+    # Fourier transform in time 
+    c = c/2
+
+    fig, axs = plot_template(2, 1, figsize = (5, 7))
+    # |u_k|^2
+    mod_fft2_u_init, k_u_init = evaluate_energy_density_spectrum(u_init, dx) 
+    
+    # generate initial condition with all the m
+    list_m = (k_u_init/(2*np.pi)*L).astype(int)
+    u_init_all_k = 0
+    for m in list_m:
+        u_init_all_k += 1/(m)**2 * u(x, L, k = m)
+    u_init = np.copy(u_init_all_k)
+
+    mod_fft2_u_init, k_u_init = evaluate_energy_density_spectrum(u_init, dx) 
+
+    ax = axs[0]
+    ax.plot(x, u_init, label = 'init')
+
+    ax = axs[1]
+    ax.set_ylabel(r'$G(k, t)$')
+    ax.scatter(k_u_init, mod_fft2_u_init/mod_fft2_u_init, label = 'init', s = 10)
+#    list_nu = np.logspace(-2, -3, 2)
+    list_nu = np.array([1e-2, 1e-3, 3e-4, 1e-4])
+
+    r_c = c * dt / ( 2 * dx )
+    
+    u_k = np.sqrt(mod_fft2_u_init[-1]) # |u_k|
+    k_m = k_u_init[-1]
+
+    t_diff = lambda nu: 1/(nu * u_k * k_m**2)
+
+    print(f'r_c: {r_c}')
+    print(f'final physic time: {N_step*dt}')
+
+    for nu in list_nu:
+        u_t = np.copy(u_init)
+        von_neumann = nu * dt /(dx**2)
+        print(f'Von Neumann factor {von_neumann:.1e}')
+
+        r_nu = nu * dt / ( dx * dx )
+        print(f'nu: {nu:.1e};   r_nu: {r_nu:.2e}')
+        print(f't diff.: {t_diff(nu):.2e}')
+        ax = axs[0]
+        for i in range(N_step):# Evolution in time
+            u_t = int_method(u_t, F, dt, c, nu, dx)
+    
+        ax.plot(x, u_t.real, label = f'nu = {nu}')
+        ax = axs[1]
+        mod_fft2_u, k_u = evaluate_energy_density_spectrum(u_t, dx)
+        ax.scatter(k_u, mod_fft2_u/mod_fft2_u_init, label = f'nu = {nu}', s = 10)
+
+    # Plot results
+    plt.suptitle(r'$\partial_t u = \nu\partial^2_x u$ $\rightarrow$ Derivata seconda con FFT + RK2')
+    plt.legend(fontsize = 9)
+    plt.tight_layout()
+#    plt.savefig('figures/diffusion/diffusion_varying_nu', dpi = 200)
+    plt.show()
+
+
+# RHS with derivative as argument
+def F_diffusion_der(u, c, nu, dx, der):
+    """
+    Function of equation:
+         du/dt = nu * d^2u/d^2x - c * du/dx = F(u)
+    """
+    return nu * der(u, dx) #- c * der.simm_der(u, dx)
+
+def analytic_sol(ampl_list, k_list, x, nu, T):
+    sol = 0
+    for A, k in zip(ampl_list, k_list):
+        sol += A * np.exp( -nu * k**2 * T) * np.sin(k*x)
+    return sol
+
+def derivative_effect(int_method, u, F):
+    #define input variable
+    # spatial inputs
+    L = 10 # Spatial size of the grid
+    N = 100 # spatial step of grid
+    
+    # temporal inputs
+    dt = 0.1 # Temporal step
+    N_step = 5000 # Number of Temporal steps
+    
+    ###############################################################
+    #define the dicrete interval dx
+    dx = L/N # step size of grid
+    x = np.linspace(0,L,N, endpoint = False)
+    
+    speed = dx/dt # speed given the values
+    c = speed
+    
+    # Define starting function
+    m = 1
+    u_init = u(x, L, k = m) # save initial condition 
+    u_t = np.copy(u_init) # create e copy to evolve it in time
+    # Fourier transform in time 
+    c = c/2
+
+    fig, axs = plt.subplots(3,1, figsize = (5,8))
+    for ax in axs:
+       ax.grid(alpha = 0.3)
+       ax.minorticks_on()
+       ax.tick_params('x', which='major', direction='in', length=5)
+       ax.tick_params('y', which='major', direction='in', length=5)
+       ax.tick_params('y', which='minor', direction='in', length=3, left=True)
+       ax.tick_params('x', which='minor', direction='in', length=3, bottom=True)
+       ax.set_xlabel('k', fontsize=15)
+       ax.set_ylabel(r'$G(k, t)$')
+       ax.set_yscale('log')
+       ax.set_xscale('log')
+ 
+    # |u_k|^2
+    mod_fft2_u_init, k_u_init = evaluate_energy_density_spectrum(u_init, dx) 
+    
+    # generate initial condition with all the m
+    list_m = (k_u_init/(2*np.pi)*L).astype(int)
+    u_init_all_k = 0
+    for m in list_m:
+        u_init_all_k += 1/(m)**2 * u(x, L, k = m)
+    u_init = np.copy(u_init_all_k)
+
+    mod_fft2_u_init, k_u_init = evaluate_energy_density_spectrum(u_init, dx) 
+
+    for ax in axs:
+        ax.scatter(k_u_init, mod_fft2_u_init/mod_fft2_u_init, label = 'init', s = 10)
+        ax.plot(k_u_init, mod_fft2_u_init/mod_fft2_u_init, lw = 0)
+
+    list_nu = np.array([1e-2, 1e-3, 3e-4, 1e-4])
+
+    r_c = c * dt / ( 2 * dx )
+    
+    u_k = np.sqrt(mod_fft2_u_init[-1]) # |u_k|
+    k_m = k_u_init[-1]
+    t_diff = lambda nu: 1/(nu * u_k * k_m**2)
+
+    print(f'r_c: {r_c}')
+    print(f'final physic time: {N_step*dt}')
+    der_methods = [der.simm_der2, der.diff_fin_comp_der2, der.fft_der2]
+    der_methods_names = ['Differenze finite simmetriche', 
+                         'Differenze finite compatte', 
+                         'Derivata con FFT']
+    for ax, der_method, title in zip(axs, der_methods, der_methods_names):
+        for nu in list_nu:
+            u_t = np.copy(u_init)
+            von_neumann = nu * dt /(dx**2)
+            r_nu = nu * dt / ( dx * dx )
+            for i in range(N_step):# Evolution in time
+                u_t = int_method(u_t, F, dt, c, nu, dx, der_method)
+        
+            mod_fft2_u, k_u = evaluate_energy_density_spectrum(u_t, dx)
+            ax.scatter(k_u, mod_fft2_u/mod_fft2_u_init, label = f'nu = {nu}', s = 6)
+
+            ana_sol = analytic_sol([1/m**2 for m in list_m], k_u_init, x, nu, dt*N_step)
+            mod_fft2_u, k_u = evaluate_energy_density_spectrum(ana_sol, dx)
+            ax.plot(k_u, mod_fft2_u/mod_fft2_u_init, lw = 0.8)
+            ax.set_title(title)
+
+    # Plot results
+    plt.suptitle(r'$\partial_t u = \nu\partial^2_x u$ $\rightarrow$ RK2')
+    plt.legend(fontsize = 9, frameon=False)
+    plt.tight_layout()
+    plt.savefig('figures/diffusion/diffusion_varying_der', dpi = 200)
+    plt.show()
+    return
+
+
 if __name__ == '__main__':
-    ampl2(Int.RK2, u_sin_hard, F_diffusion)
+    #ampl2(Int.RK2, u_sin_simple, F_diffusion)
+    #nu_effect(Int.RK2, u_sin_simple, F_diffusion)
+    derivative_effect(Int.RK2, u_sin_simple, F_diffusion_der)
---
+++ Modulo4/Progetto4/utils.py
@@ -0,0 +1,56 @@
+"""
+FFT function, energy density function and plot results function
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import fftpack
+
+
+# Fourier transform in time 
+def my_real_fft(u, dx):
+    """
+    Real Fast Fourier Transform.
+    """
+    N = len(u)
+    # x : 2 * pi = y : 1 ---> unitary rate
+    # => dy = dx/(2*pi)
+    dy =  dx / (2 * np.pi)
+    # fft(j) = (u * exp(-2*pi*i*j*np.arange(n)/n)).sum()
+    fft = fftpack.fft(u) # Discret fourier transform 
+    k = fftpack.fftfreq(N, dy) 
+    return fft, k
+
+def evaluate_energy_density_spectrum(u_t, dx):
+    # final fft
+    fft_u, k_u = my_real_fft(u_t, dx) # FFT of final u 
+    mod_u_k = np.abs(fft_u) # |u_k|
+    mod_fft2_u = (mod_u_k)**2 # |u_k|^2
+    mask_pos_k_u = k_u > 0 # mask for positive k final
+    mod_fft2_u = mod_fft2_u[mask_pos_k_u]
+    k_u = k_u[mask_pos_k_u]
+    return mod_fft2_u, k_u
+
+def plot_template(row = 1, col = 2, figsize = (12, 3)):
+    fig, axs = plt.subplots(row, col, figsize = figsize)
+    ax = axs[0]
+    ax.set_xlabel('x', fontsize=15)
+    ax.set_ylabel('u', fontsize=15)
+    ax.grid(alpha = 0.3)
+    ax.minorticks_on()
+    ax.tick_params('x', which='major', direction='in', length=5)
+    ax.tick_params('y', which='major', direction='in', length=5)
+    ax.tick_params('y', which='minor', direction='in', length=3, left=True)
+    ax.tick_params('x', which='minor', direction='in', length=3, bottom=True)
+    ax = axs[1]
+    ax.grid(alpha = 0.3)
+    ax.minorticks_on()
+    ax.tick_params('x', which='major', direction='in', length=5)
+    ax.tick_params('y', which='major', direction='in', length=5)
+    ax.tick_params('y', which='minor', direction='in', length=3, left=True)
+    ax.tick_params('x', which='minor', direction='in', length=3, bottom=True)
+    ax.set_xlabel('k', fontsize=15)
+    ax.set_ylabel(r'$\left|u_k\right|^2$', fontsize=15)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    return fig, axs
