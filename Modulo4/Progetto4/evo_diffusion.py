e173031ac57957899adcd5741181bd7ee456fb4e
dodogabrie <dodogabrie97@live.it>
2022-02-15 19:01:57 +0100

    added diff. time analysis

--- Modulo4/Progetto4/evo_diffusion.py
+++ Modulo4/Progetto4/evo_diffusion.py
@@ -278,8 +278,106 @@ def derivative_effect(int_method, u, F):
     plt.show()
     return
 
+def tau_diff_study(int_method, u, F):
+    #define input variable
+    # spatial inputs
+    L = 10 # Spatial size of the grid
+    N = 100 # spatial step of grid
+    
+    # temporal inputs
+    dt = 0.1 # Temporal step
+    N_step = 30000 # Number of Temporal steps
+    t = np.linspace(0, dt*N_step, N_step, endpoint=False)
+    
+    nu = 1e-2
+
+    ###############################################################
+    #define the dicrete interval dx
+    dx = L/N # step size of grid
+    x = np.linspace(0,L,N, endpoint = False)
+    
+    speed = dx/dt # speed given the values
+    c = speed
+    
+    # Define starting function
+    m = 1
+    u_init = u(x, L, k = m) # save initial condition 
+    u_t = np.copy(u_init) # create e copy to evolve it in time
+    # Fourier transform in time 
+    c = c/2
+
+#    fig, axs = plot_template(2, 1, figsize = (5, 7))
+    # |u_k|^2
+    mod_fft2_u_init, k_u_init = evaluate_energy_density_spectrum(u_init, dx) 
+#    ax = axs[0]
+#    ax.plot(x, u_init, label = 'init')
+
+#    ax = axs[1]
+#    ax.set_ylabel(r'$G(k, t)$')
+#    ax.scatter(k_u_init, mod_fft2_u_init/mod_fft2_u_init, label = 'init', s = 10)
+#    list_nu = np.logspace(-2, -3, 2)
+#    list_nu = np.array([1e-2, 1e-3, 3e-4, 1e-4])
+
+    
+    ################# Diff time ###############################
+    u_k = np.sqrt(mod_fft2_u_init[m-1]) # |u_k|
+    k_m = k_u_init[m-1]
+    def t_diff(nu, k_m):
+        return 1/(nu * k_m**2)
+    ###########################################################
+
+#    print(f'r_c: {r_c}')
+    print(f'final physic time: {N_step*dt}')
+
+    von_neumann = nu * dt /(dx**2)
+    print(f'Von Neumann factor {von_neumann:.1e}')
+
+    fig, ax = plt.subplots(1,1, figsize = (6, 6))
+
+    print(L * np.max(k_u_init)/ (2 * np.pi))
+    list_m = [1, 3, 15]
+    ax.plot([], [], ' ', label=fr"$\nu$ = {nu}")
+    for m in list_m:
+        u_init = u(x, L, k = m) # save initial condition 
+        k_m = 2 * np.pi / L * m
+        print(f'm : {m} --> t diff.: {t_diff(nu, k_m):.2e}')
+        u_t = np.copy(u_init)
+        list_ampl_diff = []
+        for i in range(N_step):# Evolution in time
+            u_t = int_method(u_t, F, dt, c, nu, dx)
+            mod_fft2_u, k_u = evaluate_energy_density_spectrum(u_t, dx)
+            list_ampl_diff.append(mod_fft2_u[m-1])
+
+        list_ampl_diff = np.array(list_ampl_diff) 
+        max_ampl = np.max(list_ampl_diff )
+        ax.plot(t, list_ampl_diff, label = f'm: {m}')
+        idx_t_close = np.argmin(np.abs(t - t_diff(nu, k_m)))
+        ymax = list_ampl_diff[idx_t_close] * 3
+        ymin = list_ampl_diff[idx_t_close] / 3
+        ax.vlines(x=t_diff(nu, k_m), ymin = ymin, ymax = ymax, lw = 0.8, color = 'k') 
+
+    ax.tick_params(axis='both', labelsize=11)
+    ax.grid(alpha = 0.3)
+    ax.minorticks_on()
+    ax.set_title(r'Evoluzione di $\left|u_k\right|^2$ nel tempo per diversi valori di $m$', fontsize = 13)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_ylim(1e-6, 10 * max_ampl)
+    ax.tick_params('x', which='major', direction='in', length=5)
+    ax.tick_params('y', which='major', direction='in', length=5)
+    ax.tick_params('y', which='minor', direction='in', length=3, left=True)
+    ax.tick_params('x', which='minor', direction='in', length=3, bottom=True)
+    ax.set_xlabel('t', fontsize = 15)
+    ax.set_ylabel(r'$\left|u_k\right|^2$', fontsize = 15)
+    plt.legend( fontsize = 12)
+    plt.savefig('figures/diffusion/diff_time_varying_m.png', dpi = 200)
+    plt.show()
+ 
+ 
+
 
 if __name__ == '__main__':
     #ampl2(Int.RK2, u_sin_simple, F_diffusion)
     #nu_effect(Int.RK2, u_sin_simple, F_diffusion)
-    derivative_effect(Int.RK2, u_sin_simple, F_diffusion_der)
+    #derivative_effect(Int.RK2, u_sin_simple, F_diffusion_der)
+    tau_diff_study(Int.RK2, u_sin_simple, F_diffusion)
